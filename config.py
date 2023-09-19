5bebe1f83a1813a8f0d1a4db448a12832b0351de
gitazamat <azaruslanovich@gmail.com>
2023-09-20 02:30:06 +0300

    hw2,3

---
+++ config.py
@@ -0,0 +1,13 @@
+from decouple import config
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
+TOKEN = config("TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot, storage=storage)
+ADMIN_ID = 5152697686
+BOT_PIC = '/Users/adiletsaparbek/PycharmProjects/geek_33_1_bot/media/bot_pic.jpeg'
+ANIMATION_PIC = '/Users/Lenovo/PycharmProjects/pythonProject2geeks_33-1_Python_month_3/media/joinblink-blink.gif'
+GROUP_ID = -1001886494144
+DESTINATION_DIR = '/Users/Lenovo/PycharmProjects/pythonProject2geeks_33-1_Python_month_3/media/'
---
+++ const.py
@@ -0,0 +1,10 @@
+START_TEXT = "Hello *{username}!* 😎\n" \
+             "Im your complex bot, nice to meet you\n" \
+             "U can press Start Questionnaire button " \
+             "to answer a few questions"
+
+PROFILE_CAPTION_TEXT = "Nickname: {nickname}\n" \
+                       "Bio: {bio}\n" \
+                       "Age: {age}\n" \
+                       "Occupation: {occupation}\n" \
+                       "Married: {married}\n"
